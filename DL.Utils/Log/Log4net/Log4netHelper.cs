a34aa3a77176a5adaec7417b87f75dab7f0ed8a3
duling <295611875@qq.com>
2020-04-08 14:26:18 +0800

    添加少提交的log文件夹

---
+++ DL.Utils/Log/Log4net/Log4netHelper.cs
@@ -0,0 +1,72 @@
+﻿using log4net;
+using System;
+using System.Collections.Concurrent;
+
+namespace DL.Utils.Log.Log4net
+{
+    public class Log4netHelper
+    {
+
+        private static readonly ConcurrentDictionary<Type, ILog> Loggers = new ConcurrentDictionary<Type, ILog>();
+        private static ILog GetLogger(Type source)
+        {
+            if (Loggers.ContainsKey(source))
+            {
+                return Loggers[source];
+            }
+            else
+            {
+                ILog logger = LogManager.GetLogger("Log4net",source);
+                Loggers.TryAdd(source, logger);
+                return logger;
+            }
+        }
+
+        public static void Debug(Type source, string message, params object[] ps)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsDebugEnabled)
+            {
+                logger.Debug(message);
+            }
+        }
+
+        public static void Error(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsErrorEnabled)
+            {
+                logger.Error(message);
+            }
+        }
+
+        public static void Fatal(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsFatalEnabled)
+            {
+                logger.Fatal(message);
+            }
+        }
+
+        public static void Info(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsInfoEnabled)
+            {
+                logger.Info(message);
+            }
+        }
+
+        public static void Warn(Type source, object message)
+        {
+            ILog logger = GetLogger(source);
+            if (logger.IsWarnEnabled)
+            {
+                logger.Warn(message);
+            }
+        }
+
+
+    }
+}
---
+++ DL.Utils/Log/Nlog/NLogHelper.cs
@@ -0,0 +1,50 @@
+﻿using NLog;
+using System;
+
+namespace DL.Utils.Log.Nlog
+{
+    public class NLogHelper
+    {
+
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+        public static void Debug(Type source, string message, params object[] ps)
+        {
+            if (logger.IsErrorEnabled)
+            {
+                logger.Debug(message, ps);
+            }
+        }
+
+        public static void Error(object message)
+        {
+            if (logger.IsErrorEnabled)
+            {
+                logger.Error(message);
+            }
+        }
+
+        public static void Fatal(object message)
+        {
+            if (logger.IsFatalEnabled)
+            {
+                logger.Fatal(message);
+            }
+        }
+
+        public static void Info(object message)
+        {
+            if (logger.IsInfoEnabled)
+            {
+                logger.Info(message);
+            }
+        }
+
+        public static void Warn(object message)
+        {
+            if (logger.IsWarnEnabled)
+            {
+                logger.Warn(message);
+            }
+        }
+    }
+}
